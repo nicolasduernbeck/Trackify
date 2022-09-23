8fab5bc24cb7766c0a3db8ad7a10c31ec952789d
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-09-23 23:09:54 +0200

    Updated model to handle whitespace 🔨

--- src/js/model.js
+++ src/js/model.js
@@ -9,6 +9,7 @@ export const state = {
 
 export const getApiData = async ip => {
   try {
+    ip = ip.trim();
     const data = await getJson(`http://ip-api.com/json/${ip}`);
     if (data.status === 'fail') throw new Error('Invalid IP - Adress!');
     console.log(data);
